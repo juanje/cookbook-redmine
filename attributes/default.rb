a9c10209c42bf2300f9112de73bb1f74d26cfdc7
Roberto Majadas <roberto.majadas@openshine.com>
2013-04-06 02:56:43 +0200

    Rewrite recipe/default.rb. Add install_method attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ default["redmine"]["repo"]      = 'git://github.com/redmine/redmine.git'
 default["redmine"]["revision"]  = '2.2.3'
 default["redmine"]["deploy_to"] = '/opt/redmine'
 default["redmine"]["path"]      = '/var/www/redmine'
+default["redmine"]["install_method"] = "source"
 
 # databases
 default["redmine"]["databases"]["production"]["adapter"]  = 'mysql'
