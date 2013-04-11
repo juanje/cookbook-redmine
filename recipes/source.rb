22dbb425b86e8d2d4c3dfa9e488708aca98177f0
Roberto Majadas <roberto.majadas@openshine.com>
2013-04-11 14:14:57 +0200

    Check ruby version from host directly

--- recipes/source.rb
+++ recipes/source.rb
@@ -180,9 +180,7 @@ deploy_revision node['redmine']['deploy_to'] do
       variables(
                 :host => 'localhost',
                 :databases => node['redmine']['databases'],
-                :rails_env => node['redmine']['env'],
-                :revision => node['redmine']['revision'],
-                :ruby_version => RUBY_VERSION
+                :rails_env => node['redmine']['env']
                 )
     end
 
